d1144974f36ab33f1b956dda5df2b549b05a19fc
Leo <leo.cabral1016@gmail.com>
2021-03-04 13:28:13 -0300

    auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,33 +11,42 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    # DEFECTO
+    #---------------------------------------------------#
+        /**
+         * The attributes that are mass assignable.
+         *
+         * @var array
+         */
+        protected $fillable = [
+            'name',
+            'email',
+            'password',
+        ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+        /**
+         * The attributes that should be hidden for arrays.
+         *
+         * @var array
+         */
+        protected $hidden = [
+            'password',
+            'remember_token',
+        ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+        /**
+         * The attributes that should be cast to native types.
+         *
+         * @var array
+         */
+        protected $casts = [
+            'email_verified_at' => 'datetime',
+        ];
+
+    # RELATIONS
+    #---------------------------------------------------#
+        // Relación One To Many
+        public function images(){
+            return $this->hasMany('App\Image');
+        }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,38 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Models\Image;
 
 Route::get('/', function () {
+
+    /*
+        $images = Image::all();
+        foreach($images as $image){
+            echo $image->image_path."<br>";
+            echo $image->description."<br>";
+            echo $image->user->name.' '.$image->user->surname."<br>";
+
+            if(count($image->comments) >= 1){
+                echo '<h4>Comentarios</h4>';
+                foreach($image->comments as $comment){
+                    echo $comment->user->name.' '.$comment->user->surname.': ';
+                    echo $comment->content."<br>";
+                }
+            }
+
+            echo 'LIKES: '.count($image->likes);
+
+            echo "<hr>";
+        }
+
+        die();
+    */
+
+
+
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
